8ebed2a585c4decf39e9e51d9f4a7e0d95a9c204
kemal <kemal.z.a@icloud.com>
2026-07-02 19:10:41 +0300

    Initial Commit

---
+++ CareMom2/CareMom2App.swift
@@ -0,0 +1,17 @@
+//
+//  CareMom2App.swift
+//  CareMom2
+//
+//  Created by Kemal Zeitulaev on 02.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct CareMom2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CareMom2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CareMom2
+//
+//  Created by Kemal Zeitulaev on 02.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
